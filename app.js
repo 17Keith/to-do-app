c71bfb3b0a88e8b4274708c4030a6986d7904ca3
17Keith <kayomega7@gmail.com>
2024-02-23 19:20:17 +0300

    Add a function that adds and removes a task

--- app.js
+++ app.js
@@ -3,11 +3,31 @@ const listContainer = document.getElementById("list-container");
 
 function addTask() {
     if (inputBox.value == '') {
-        alert("Kindly add a new task")
+        alert("Kindly add a new task");
     }
     else {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
-        listContainer.appendChild(li)
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
+    inputBox.value = "";
 }
+
+// Add task when Enter key is pressed
+inputBox.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        addTask();
+    }
+});
+
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+    }
+}, false);
